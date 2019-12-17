239c49062c16a8b39e60dab513e9c3450227a1ea
Michael Redig <mredig@gmail.com>
2019-12-16 22:16:29 -0600

    setup storyboard

---
+++ Drugitol/DrugEntryVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Drugitol
+//
+//  Created by Michael Redig on 12/16/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class DrugEntryVC: UIViewController {
+
+}
+
+extension DrugEntryVC: UITableViewDelegate, UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+}
