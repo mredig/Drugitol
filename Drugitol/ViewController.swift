673523ba4504c7ea920ede2f5cdf1f5bbe5fe71d
Michael Redig <mredig@gmail.com>
2019-12-16 21:54:49 -0600

    initial commit

---
+++ Drugitol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drugitol
+//
+//  Created by Michael Redig on 12/16/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
